ea65e82130afd5064351071b85343bf7a07a60bf
jishan <imranislamjishan80@gmail.com>
2024-06-15 22:29:22 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import {View, Text, SafeAreaView, ScrollView} from 'react-native';
+import React from 'react';
+
+const App = (): JSX.Element => {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <Text>App</Text>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default App;
